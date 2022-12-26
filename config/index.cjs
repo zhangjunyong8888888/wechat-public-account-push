db8aa298e502053e52e47eff7357a555edf35da7
zhangjunyong8888888 <44471442+zhangjunyong8888888@users.noreply.github.com>
2022-12-26 15:01:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '45786338891191bab53c808746f1198c',
 
-  PROVINCE: '山西',
-  CITY: '临汾',
+  PROVINCE: '北京',
+  CITY: '昌平',
 
   USERS: [
     {
@@ -25,18 +25,14 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'KWl6Po4NlNhmtUGx8Wrmc2XmK2Kh2Le8nS4Hph_sGGE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '12-28',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
           type: '*生日', name: '宝贝', year: '1997', date: '11-29',
         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2014', date: '03-16',
+          type: '节日', name: '2023年春节', year: '2023', date: '01-22',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -44,7 +40,7 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2014-03-16' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2023-05-01' },
       ],
     },
   ],
