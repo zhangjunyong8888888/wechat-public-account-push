8a3ecb840d9bdb3efb4cbd324c5a4f754fb4122e
zhangjunyong8888888 <44471442+zhangjunyong8888888@users.noreply.github.com>
2022-12-26 15:04:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '钰钰',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o06lF6j7Bc71IPATC9g4skG0tj3o',
+      id: 'o06lF6slpCg4i-u0bogK0xau9xM8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'KWl6Po4NlNhmtUGx8Wrmc2XmK2Kh2Le8nS4Hph_sGGE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -29,10 +29,13 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1997', date: '11-29',
+          type: '*生日', name: '你的', year: '1997', date: '11-29',
         },
         {
           type: '节日', name: '2023年春节', year: '2023', date: '01-22',
+        },
+        {
+          type: '*生日', name: '辣条生日', year: '2022', date: '11-29',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
